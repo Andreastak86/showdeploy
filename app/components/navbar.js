019ecbc5be617ee6f2083aa38a9f2e56b179c6ed
Andreastak86 <andreas.takvam@gmail.com>
2025-03-07 10:24:01 +0100

    first commit

---
+++ app/components/navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div>
+            <h1>This is the navbar</h1>
+            <p>This is a simple navigation bar</p>
+        </div>
+    );
+}
